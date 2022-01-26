4f349c1b83d8447a87848a12a91215fcd861e540
Okike Chinonso <Chinonsookike@gmail.com>
2022-01-26 16:30:21 +0100

    added database url

--- pkg/database/sqlcon.go
+++ pkg/database/sqlcon.go
@@ -16,7 +16,7 @@ func MySqlCon() (db *sql.DB, err error) {
 	//
 	//psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
-	db, err = sql.Open("postgres", "postgres://zcxydxjqbdeeqe:4d3c5682ea4de184ab2e59f26b5ff9981196ab729e615da5a8d91056198f5c4c@ec2-63-32-7-190.eu-west-1.compute.amazonaws.com:5432/dch1l4n831j80m")
+	db, err = sql.Open("postgres", "postgres://megqyyfdthyzrz:0fdfa9b60a54af403c68817f80eef83f5999248222e00dd1a3b160d3760d3ec6@ec2-54-209-165-105.compute-1.amazonaws.com:5432/da92eirikddah1")
 	if err != nil {
 		return nil, err
 	}
